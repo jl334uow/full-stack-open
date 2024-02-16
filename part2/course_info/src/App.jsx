d96ee467124781d84c80adc0e694834c4f9e3f6c
Justin <lljustinlu@gmail.com>
2024-02-16 12:15:20 +1100

    Exercise 2.5 Export Course component to separate module

--- part2/course_info/src/App.jsx
+++ part2/course_info/src/App.jsx
@@ -1,33 +1,4 @@
-const Header = ({head}) => {
-  return(
-    <>
-      <h2>{head}</h2>
-    </>
-  )
-}
-
-const Content = ({parts}) => {
-  return(
-    <>
-      {parts.map(c => <p key={c.id}>{c.name} {c.exercises}</p>)}
-      <p><b>total of {parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)} exercises</b></p>
-    </>
-  )
-}
-
-const Course = ({courses}) => {
-  return(
-    <>
-      <h1>Web development curriculum</h1>
-      <Header head={courses[0].name}/>
-      <Content parts={courses[0].parts}/>
-
-      <Header head={courses[1].name}/>
-      <Content parts={courses[1].parts}/>
-
-    </>
-  )
-}
+import Course from './Course.jsx';
 
 const App = () => {
   const courses = [
