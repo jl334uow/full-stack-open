811808f15c1d65dbb99fb2df754caa54c1f23fee
Justin <lljustinlu@gmail.com>
2024-02-17 21:51:18 +1100

    Exercise 2.6 Phonebook
    Create a form with an input for name

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+  const [newName, setNewName] = useState('')
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('Name added', event.target, newName)
+    const personsCopy = [...persons]
+    personsCopy.push({name: newName})
+    setPersons(personsCopy)
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return(
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <>{person.name}<br/></>)}
+    </div>
+  )
+}
+
+export default App
