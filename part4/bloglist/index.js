967f4a4d542bbd4fa401b5a425a043544c18fe7f
Justin Lս <lljustinlu@gmail.com>
2024-02-27 15:20:42 +1100

    Exercise 4.8 Supertest and refactor bloglist app

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,14 +1,17 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-app.use(cors())
-app.use(express.json())
-
+const app = require('./app')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
 
-app.use('/api/blogs', blogsRouter)
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// app.use(cors())
+// app.use(express.json())
+
+// const config = require('./utils/config')
+// const blogsRouter = require('./controllers/blogs')
+
+// app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose')
 
-const mongoUrl = process.env.MONGODB_URI
-
-console.log('connecting to', mongoUrl)
-
-mongoose.connect(mongoUrl)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const blogSchema = new mongoose.Schema({
     title: String,
