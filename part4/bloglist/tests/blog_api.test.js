03cfcb8271c3908c336533f2fc90ca2f942531eb
Justin Lս <lljustinlu@gmail.com>
2024-02-28 14:57:03 +1100

    Exercise 4.9 verify unique id of the blog post

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,4 +1,5 @@
-const { test, after } = require('node:test')
+const { test, after, describe } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app.js')
@@ -15,3 +16,14 @@ test('blogs are returned as json', async () => {
 after(async () => {
   await mongoose.connection.close()
 })
+
+describe('check id', () => {
+    test('check _id and id is the same', async () => {
+      const response = await api.get('/api/blogs')
+      console.log("BOBS YA UNCLE" + JSON.stringify(response.body))
+      response.body.map(e => assert(e._id === e.id))
+    })
+    after(async () => {
+        await mongoose.connection.close()
+      })
+  })
