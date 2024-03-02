6d4c79a1def63c24cf52d130b0f885132261a977
Justin Lս <lljustinlu@gmail.com>
2024-03-02 22:29:02 +1100

    Exercise 4.15 Create and view users Implement a way to create new users by doing a HTTP POST request to address api/users. Users have a username, password and name.
    Do not save passwords to the database as clear text, but use the bcrypt
    library.

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,18 +1,6 @@
 const app = require('./app')
 const config = require('./utils/config')
 
-// require('dotenv').config()
-// const express = require('express')
-// const app = express()
-// const cors = require('cors')
-// app.use(cors())
-// app.use(express.json())
-
-// const config = require('./utils/config')
-// const blogsRouter = require('./controllers/blogs')
-
-// app.use('/api/blogs', blogsRouter)
-
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
