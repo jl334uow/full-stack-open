3e52b5c30bdf5f645b41d8b7b88304c32b930390
Justin Lս <lljustinlu@gmail.com>
2024-03-06 22:26:10 +1100

    Exercise 4.18 Implement token-based authentication

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', (request, response) => {
     Blog.find({}).then(blogs => {
@@ -15,20 +24,28 @@ blogsRouter.get('/api/blogs', (request, response) => {
     })
 })
 
-blogsRouter.post('/api/blogs', (request, response) => {
+blogsRouter.post('/api/blogs', async (request, response) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
     if (body.title && body.author && body.url){
       const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes || 0
+        likes: body.likes || 0,
+        user: user.id
       })
   
-      blog.save().then(result => {response.status(201).json(result)}).catch(error => next(error))
+      await blog.save().then(result => {response.status(201).json(result)}).catch(error => next(error))
+
     }
     else {
-      blog.abort().then(() => response.status(400))
+      return response.status(400).json({ error: `missing value(s) in request` })
     }
 })
 
--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -12,6 +12,21 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 3,
+    },
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
     }
 })
 
