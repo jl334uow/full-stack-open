cd774602c155b9334cc26f629e1c3636dcace4d2
Justin Lս <lljustinlu@gmail.com>
2024-03-14 14:26:07 +1100

    Ex 5.2 Implement logout & make login permanent
    Use local storage to make login permanent

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -16,6 +16,15 @@ const App = () => {
       setBlogs( blogs )
     )  
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  },[])
   const loginForm = () => {
     return (<form onSubmit={handleLogin}>
       <h2>Login</h2>
@@ -89,6 +98,7 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -108,7 +118,11 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
+      <form onSubmit={() => {
+        window.localStorage.removeItem('loggedBlogappUser')
+      }}>
+        <p>{user.name} logged-in <button type="submit">logout</button></p>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
